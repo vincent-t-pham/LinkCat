f1b3623dd5c8ef024689c2b3f20950ea0cd71014
vincent-t-pham <vincent.t.pham@sjsu.edu>
2023-07-26 20:27:23 -0700

    changed some names

--- linkopener.py
+++ linkopener.py
@@ -1,12 +1,9 @@
-print('hello')
-
 from flask import Flask, render_template, request
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 import webbrowser
-
 
 app = Flask(__name__)
 
@@ -17,9 +14,9 @@ linkStorage = {}
 def makeLink():
     if request.method == 'POST':
         print('Start of makeLink')
-        # names = request.form.getlist('names')  # Assuming links are passed as a list in a form field
+        links = request.form.getlist('names')  # Assuming links are passed as a list in a form field
         links = ['http://www.google.com', 'http://www.amazon.com', 'http://www.facebook.com']
-        linkID = generateLink()
+        linkID = randomLinkID()
 
         linkStorage[linkID] = links
 
@@ -35,26 +32,22 @@ def makeLink():
 
 
 @app.route('/<linkID>')
-def executeScript(linkID):
+def clickedLink(linkID):
     if linkID in linkStorage:
-        # new = 1
-        # Retrieve the names associated with the link identifier
+ 
         links = linkStorage[linkID]  
 
         for link in links:
-            webbrowser.open(link, new=0)           # If new is 0, the url is opened in the same browser window if possible. 
-                                                    # If new is 1, a new browser window is opened if possible. If new is 2, 
-                                                    # a new browser page (“tab”) is opened if possible
+            webbrowser.open(link, new=0)                # If new is 0, the url is opened in the same browser window if possible. 
+                                                        # If new is 1, a new browser window is opened if possible. If new is 2, 
+                                                        # a new browser page (“tab”) is opened if possible
             # if(new==1): new=0
 
-        # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-        # Needs webdriver code to open the links in a browser #
-        # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-        return render_template('printLinks.html', links=links)
+        return render_template('forcePageClose.html')   # Closes the page that was opened from the link
     else:
         return 'Invalid link.'
 
-def generateLink():
+def randomLinkID():
     import random
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
     # NEEDS to check if a unique linkID has been generated  #

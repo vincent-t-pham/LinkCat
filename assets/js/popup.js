98fec7b8017450aad265e2c1737f465a3502ccc1
vincent-t-pham <59778724+vincent-t-pham@users.noreply.github.com>
2023-10-20 03:11:34 -0700

    Add textbox functionality

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -2,12 +2,30 @@
 document.addEventListener("DOMContentLoaded", function(){
     const myButton = document.querySelector("#test-button");
     const generateLink = document.querySelector("#submit");
-    
+    const textboxContainer = document.getElementById("textboxContainer");
+
     const resultElement = document.createElement('p');
     resultElement.id = 'result';
 
     myButton.addEventListener("click", function(){
-        var div = document.getElementsByTagName("body")[0].style.backgroundColor = "aqua";
+        // var div = document.getElementsByTagName("body")[0].style.backgroundColor = "aqua";
+
+        const newLabel = document.createElement('label');
+        newLabel.setAttribute('for', 'textbox' + (textboxContainer.childElementCount / 2 + 1));
+        newLabel.textContent = 'Textbox ' + (textboxContainer.childElementCount / 2 + 1) + ':';
+        // Create a new input element
+        const newInput = document.createElement('input');
+        newInput.setAttribute('type', 'text');
+        newInput.setAttribute('id', 'textbox' + (textboxContainer.childElementCount / 2 + 1));
+        newInput.setAttribute('name', 'textbox' + (textboxContainer.childElementCount / 2 + 1));
+
+        // Append the new label and input to the container
+        textboxContainer.appendChild(newLabel);
+        textboxContainer.appendChild(newInput);
+
+
+        const newTextbox = document.createElement('');
+        textboxContainer.appendChild()
         console.log("popup.js test");
     });
 
@@ -53,7 +71,36 @@ document.addEventListener("DOMContentLoaded", function(){
         } catch (error) {
             console.error('ErrOR:', error);
         }
+
+        
+
     });
+
+
+    // const copyLink = document.getElementById('copyLink');
+
+    // copyLink.addEventListener('click', function (e) {
+    //     e.preventDefault(); // Prevent the link from navigating
+
+    //     const urlToCopy = copyLink.href; // Get the URL from the link's href attribute
+
+    //     // Create a temporary input element to hold the URL
+    //     const tempInput = document.createElement('input');
+    //     tempInput.value = urlToCopy;
+
+    //     // Append the input to the document
+    //     document.body.appendChild(tempInput);
+
+    //     // Select the text in the input and copy it to the clipboard
+    //     tempInput.select();
+    //     document.execCommand('copy');
+
+    //     // Remove the input from the document
+    //     document.body.removeChild(tempInput);
+
+    //     alert('URL copied to clipboard: ' + urlToCopy);
+    // });
+
 
     // generateLink.addEventListener("click", function(){
     //     console.log("Trying to POST pressed");
